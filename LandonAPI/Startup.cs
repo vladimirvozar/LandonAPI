b0c8ae21db5843f58e7699ea98ee1a4dea4e5c8e
Vladimir Vozar <vladimirvozar@gmail.com>
2019-12-31 12:17:43 +0100

    Disabled browser launching when running project. Routing urls set to lowercase

--- LandonAPI/Startup.cs
+++ LandonAPI/Startup.cs
@@ -26,6 +26,7 @@ namespace LandonAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddRouting(options => options.LowercaseUrls = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
