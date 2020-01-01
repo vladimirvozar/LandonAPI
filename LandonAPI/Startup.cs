f705488ba6cbb3baf07f9627637485b898bef88c
Vladimir Vozar <vladimirvozar@gmail.com>
2020-01-01 12:49:45 +0100

    Serialize exceptions as JSON

--- LandonAPI/Startup.cs
+++ LandonAPI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LandonAPI.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,7 +28,13 @@ namespace LandonAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services
+                .AddMvc(options =>
+                {
+                    options.Filters.Add<JsonExceptionFilter>();
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
             services.AddRouting(options => options.LowercaseUrls = true);
 
             services.AddApiVersioning(options =>
