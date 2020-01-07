440f3c1a079f5b9cd83723ed31d85fdd9ba6f421
Vladimir Vozar <vladimirvozar@gmail.com>
2020-01-07 10:58:15 +0100

    Rewrite resource HREFs

--- LandonAPI/Infrastructure/MappingProfile.cs
+++ LandonAPI/Infrastructure/MappingProfile.cs
@@ -11,8 +11,12 @@ namespace LandonAPI.Infrastructure
     {
         public MappingProfile()
         {
-            CreateMap<RoomEntity, Room>().ForMember(dest => dest.Rate, opt => opt.MapFrom(src => src.Rate / 100.0m));
-            // TODO Url.Link
+            CreateMap<RoomEntity, Room>()
+                .ForMember(dest => dest.Rate, opt => opt.MapFrom(src => src.Rate / 100.0m))
+                .ForMember(dest => dest.Self, opt => opt.MapFrom( src =>
+                    Link.To(
+                        nameof(Controllers.RoomsController.GetRoomById), 
+                        new { roomId = src.Id})));
         }
     }
 }
